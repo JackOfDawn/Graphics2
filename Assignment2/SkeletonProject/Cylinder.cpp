bf03d7461365f392a57834ede5bb1f1d8ff77949
Scott Barrett <scott.barrett@mymail.champlain.edu>
2015-02-06 19:51:07 -0500

    Corrected the cylinder

--- Assignment2/SkeletonProject/Cylinder.cpp
+++ Assignment2/SkeletonProject/Cylinder.cpp
@@ -13,7 +13,7 @@ sideFacetsNum(sideFacetsNum)
 
 void Cylinder::buildDemoCubeVertexBuffer(IDirect3DDevice9* gd3dDevice)
 {
-	const int NUM_VERTICES = sideFacetsNum * 2;
+	const int NUM_VERTICES = sideFacetsNum * 2 + 2; // 2 centerpoints
 	m_NumVertices = NUM_VERTICES;
 
 	HR(gd3dDevice->CreateVertexBuffer(NUM_VERTICES * sizeof(VertexPos), D3DUSAGE_WRITEONLY,
@@ -34,6 +34,10 @@ void Cylinder::buildDemoCubeVertexBuffer(IDirect3DDevice9* gd3dDevice)
 			remainingDegrees -= deltaDegrees;
 		}
 
+		assert(i < NUM_VERTICES);
+		v[i] = VertexPos(0, 0, 0);
+		++i;
+
 		remainingDegrees = PI * 2;
 
 		while (remainingDegrees > 0)
@@ -44,6 +48,10 @@ void Cylinder::buildDemoCubeVertexBuffer(IDirect3DDevice9* gd3dDevice)
 			remainingDegrees -= deltaDegrees;
 		}
 
+		assert(i < NUM_VERTICES);
+		v[i] = VertexPos(0, height, 0);
+		++i;
+
 		assert(i == NUM_VERTICES);
 	}
 
@@ -52,10 +60,16 @@ void Cylinder::buildDemoCubeVertexBuffer(IDirect3DDevice9* gd3dDevice)
 
 void Cylinder::buildDemoCubeIndexBuffer(IDirect3DDevice9* gd3dDevice)
 {
-	const int NUM_TRIANGLES_PER_CIRCLE = (sideFacetsNum - 2);
+	const int NUM_TRIANGLES_PER_CIRCLE = sideFacetsNum;
 	const int NUM_SIDE_TRIANGLES = sideFacetsNum * 2;
 	const int NUM_TRIANGLES = NUM_TRIANGLES_PER_CIRCLE * 2 + NUM_SIDE_TRIANGLES;
 	const int NUM_INDICES = NUM_TRIANGLES * 3;
+
+	const int END_OF_CIRCLE_VERTICES = (m_NumVertices / 2) - 1;
+	const int START_SECOND_CIRCLE_VERTICES = END_OF_CIRCLE_VERTICES + 1;
+	const int FIRST_CENTERPOINT = (m_NumVertices / 2) - 1;
+	const int SECOND_CENTERPOINT = m_NumVertices - 1;
+
 	m_NumTriangles = NUM_TRIANGLES;
 
 	HR(gd3dDevice->CreateIndexBuffer(NUM_INDICES * sizeof(WORD), D3DUSAGE_WRITEONLY,
@@ -68,20 +82,16 @@ void Cylinder::buildDemoCubeIndexBuffer(IDirect3DDevice9* gd3dDevice)
 	addTriangle.addIndex = &addIndex;
 
 	// Draw circle triangles
+	for (int i = 0; i < NUM_TRIANGLES_PER_CIRCLE; ++i)
 	{
-		for (int i = 0; i < NUM_TRIANGLES_PER_CIRCLE; ++i)
-		{
-			addTriangle((i + 2) % (m_NumVertices / 2), (i + 1) % (m_NumVertices / 2), 0);
-			addTriangle(sideFacetsNum, (((i + 1) % m_NumVertices) + sideFacetsNum) % m_NumVertices, (((i + 2) % m_NumVertices) + sideFacetsNum) % m_NumVertices);
-		}
+		addTriangle((i + 2) % END_OF_CIRCLE_VERTICES, (i + 1) % END_OF_CIRCLE_VERTICES, FIRST_CENTERPOINT);
+		addTriangle(SECOND_CENTERPOINT, (i + START_SECOND_CIRCLE_VERTICES) % SECOND_CENTERPOINT, max((i + 1 + START_SECOND_CIRCLE_VERTICES) % SECOND_CENTERPOINT, START_SECOND_CIRCLE_VERTICES));
 	}
 
+	for (int i = 0; i < NUM_SIDE_TRIANGLES / 2; ++i)
 	{
-		for (int i = 0; i < NUM_SIDE_TRIANGLES / 2; ++i)
-		{
-			addTriangle((i + m_NumVertices / 2) % m_NumVertices, (i + 1) % (m_NumVertices / 2), max((i + 1 + m_NumVertices / 2) % m_NumVertices, m_NumVertices / 2));
-			addTriangle((i + 1) % (m_NumVertices / 2), (i + m_NumVertices / 2) % m_NumVertices, i % (m_NumVertices / 2));
-		}
+		addTriangle((i + START_SECOND_CIRCLE_VERTICES) % m_NumVertices, (i + 1) % END_OF_CIRCLE_VERTICES, max((i + 1 + START_SECOND_CIRCLE_VERTICES) % SECOND_CENTERPOINT, START_SECOND_CIRCLE_VERTICES));
+		addTriangle((i + 1) % END_OF_CIRCLE_VERTICES, (i + START_SECOND_CIRCLE_VERTICES) % m_NumVertices, i % END_OF_CIRCLE_VERTICES);
 	}
 
 	// Make sure we're not drawing too many triangles
